4f70a0228eaade0b4761d61000afa40c58c1c38b
Julie <zaq5491@gmail.com>
2023-02-07 14:51:40 +0800

    modify questsion1

--- first.py
+++ first.py
@@ -6,14 +6,44 @@ def main():
     for i in name_list:
         n = int(input(i + "的原始分數為:"))
         score_list.append(n)
+    print("\n")
+
 
     for name, score in zip(name_list, score_list):
-        if score < 40:
-            score = score
-        elif score % 5 <= 3:
-            temp = 5 - (score % 5)
+        ori = score
+        temp = 5 - (score % 5)
+        if (temp <= 3) :
             score = score + temp
-        print(name + "調整後分數為:" + str(score))
+        if score < 40:
+            print(name + "調整後分數為:" + str(ori))
+        else:
+            print(name + "調整後分數為:" + str(score))
+
+
+
 if __name__ == '__main__':
     main()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for name, score in zip(name_list, score_list):
+#         if score < 40:
+#             score = score
+#             temp = 5 - (score % 5)
+#             if (temp <= 3): score = score + temp
+#         print(name + "調整後分數為:" + str(score))

b0b13d069d392fa7891a6cd3820bb815590a9e8a
wujulie <zaq5491@gmail.com>
2023-02-08 22:44:28 +0800

    清除多餘的註解

--- autotest/main.py
+++ autotest/main.py
@@ -3,18 +3,15 @@ from  time import sleep
 from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
 options.add_experimental_option('androidPackage', 'com.android.chrome')
-server = 'http://localhost:4723/wd/hub'
-desired_caps = { 'platformName': 'Android', 'deviceName': 'CPH1877', 'appPackage': 'com.example.myapplication', 'appActivity': '.MainActivity' }
-
 driver = webdriver.Chrome('./chromedriver', options=options)
 driver.get('https://www.cathaybk.com.tw/cathaybk')
 driver.implicitly_wait(10)
-driver.save_screenshot('home.png')
+# driver.save_screenshot('home.png')
 driver.find_element_by_xpath('/html/body/div[1]/header/div/div[1]/a/img[2]').click()#點擊個人金融
 driver.find_element_by_xpath('/html/body/div[1]/header/div/div[3]/div/div[2]/div/div/div[1]/div[1]').click()
 driver.find_element_by_xpath('/html/body/div[1]/header/div/div[3]/div/div[2]/div/div/div[1]/div[2]/div/div[1]').click()
 driver.implicitly_wait(10)
-driver.save_screenshot('credit_card_item.png')
+# driver.save_screenshot('credit_card_item.png')
 credit_card_item = driver.find_elements(By.XPATH,"//div[contains(@class,'is-L2open')]//a[contains(@class,'cubre-a-menuLink')]")
 print("信用卡項目: "+str(len(credit_card_item)))
 sleep(7)
@@ -28,7 +25,7 @@ stop_card = "/html/body/div[1]/main/article/section[6]/div/div[2]/div/div[2]" #X
 for i in range(1, len(stop_card_list)+1):
     driver.find_element_by_xpath("/html/body/div[1]/main/article/section[6]/div/div[2]/div/div[2]" + "/span[%d]"%(i)).click()
     sleep(10)
-    driver.save_screenshot('stop_card_{}'.format(i) + ".png")
+    # driver.save_screenshot('stop_card_{}'.format(i) + ".png")
     sleep(10)
 
 

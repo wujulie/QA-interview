d3bee519dfcf28aa0f77ed434b0c59d128f121eb
wujulie <zaq5491@gmail.com>
2023-02-08 22:39:02 +0800

    modify clean code

--- autotest/main.py
+++ autotest/main.py
@@ -1,21 +1,20 @@
-
 from selenium import webdriver
 from  time import sleep
 from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
 options.add_experimental_option('androidPackage', 'com.android.chrome')
 server = 'http://localhost:4723/wd/hub'
-desired_caps = { 'platformName': 'Android', 'deviceName': 'MI_NOTE_Pro', 'appPackage': 'com.tencent.mm', 'appActivity': '.ui.LauncherUI' }
+desired_caps = { 'platformName': 'Android', 'deviceName': 'CPH1877', 'appPackage': 'com.example.myapplication', 'appActivity': '.MainActivity' }
 
 driver = webdriver.Chrome('./chromedriver', options=options)
 driver.get('https://www.cathaybk.com.tw/cathaybk')
 driver.implicitly_wait(10)
-# driver.save_screenshot('home.png')
+driver.save_screenshot('home.png')
 driver.find_element_by_xpath('/html/body/div[1]/header/div/div[1]/a/img[2]').click()#點擊個人金融
 driver.find_element_by_xpath('/html/body/div[1]/header/div/div[3]/div/div[2]/div/div/div[1]/div[1]').click()
 driver.find_element_by_xpath('/html/body/div[1]/header/div/div[3]/div/div[2]/div/div/div[1]/div[2]/div/div[1]').click()
 driver.implicitly_wait(10)
-# driver.save_screenshot('credit_card_item.png')
+driver.save_screenshot('credit_card_item.png')
 credit_card_item = driver.find_elements(By.XPATH,"//div[contains(@class,'is-L2open')]//a[contains(@class,'cubre-a-menuLink')]")
 print("信用卡項目: "+str(len(credit_card_item)))
 sleep(7)
